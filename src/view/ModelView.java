d57528b5ed4f31882530905365353537ba768a93
Sariaka Razakandisa <sariakaraz8@gmail.com>
2025-11-09 16:26:41 +0300

    sprint 4 bis, modelView, dispatcher

---
+++ src/view/ModelView.java
@@ -0,0 +1,16 @@
+package view;
+
+public class ModelView {
+    private String view;
+    public ModelView(String view) {
+        this.view = view;
+    }
+    public ModelView() {
+    }
+    public String getView() {
+        return view;
+    }
+    public void setView(String view) {
+        this.view = view;
+    }
+}
